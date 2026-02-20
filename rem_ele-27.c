620d83ce9cd87d4832d6df95b0297742018c89ab
Priyadharshini09-10 <priyadharshini09105@gmail.com>
2026-02-20 10:27:15 +0530

    rem_ele-27.c

--- rem_ele-27.c
+++ rem_ele-27.c
@@ -11,9 +11,15 @@ return k;
 }
 
 int main(){
-int nums[]={0,1,2,2,3,0,4,2};
-int val=2;
-int n=sizeof(nums)/sizeof(nums[0]);
+int n,val;
+printf("Enter size:");
+scanf("%d",&n);
+int nums[n];
+printf("Enter elements:");
+for(int i=0;i<n;i++)
+scanf("%d",&nums[i]);
+printf("Enter value to remove:");
+scanf("%d",&val);
 int k=removeElement(nums,n,val);
 printf("Remaining count:%d\n",k);
 printf("Array after removing:\n");
